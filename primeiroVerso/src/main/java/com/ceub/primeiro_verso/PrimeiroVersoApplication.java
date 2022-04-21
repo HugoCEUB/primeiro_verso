37b8b0dfb09c37076f5d8d05d4ee37e583fe0317
Hugo Neres <neres.hugo@eb.mil.br>
2022-04-21 17:24:38 -0300

    Pedido, EstadoPagamento e Pagamento

--- primeiroVerso/src/main/java/com/ceub/primeiro_verso/PrimeiroVersoApplication.java
+++ primeiroVerso/src/main/java/com/ceub/primeiro_verso/PrimeiroVersoApplication.java
@@ -1,5 +1,6 @@
 package com.ceub.primeiro_verso;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,17 @@ import com.ceub.primeiro_verso.domain.Cidade;
 import com.ceub.primeiro_verso.domain.Cliente;
 import com.ceub.primeiro_verso.domain.Endereco;
 import com.ceub.primeiro_verso.domain.Estado;
+import com.ceub.primeiro_verso.domain.Pagamento;
+import com.ceub.primeiro_verso.domain.PagamentoComBoleto;
+import com.ceub.primeiro_verso.domain.PagamentoComCartao;
+import com.ceub.primeiro_verso.domain.Pedido;
 import com.ceub.primeiro_verso.domain.Produto;
+import com.ceub.primeiro_verso.domain.enums.EstadoPagamento;
 import com.ceub.primeiro_verso.domain.enums.TipoCliente;
 import com.ceub.primeiro_verso.repositories.CategoriaRepository;
 import com.ceub.primeiro_verso.repositories.EstadoRepository;
+import com.ceub.primeiro_verso.repositories.PagamentoRepository;
+import com.ceub.primeiro_verso.repositories.PedidoRepository;
 import com.ceub.primeiro_verso.repositories.ProdutoRepository;
 import com.ceub.primeiro_verso.repositories.CidadeRepository;
 import com.ceub.primeiro_verso.repositories.ClienteRepository;
@@ -41,6 +49,12 @@ public class PrimeiroVersoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroVersoApplication.class, args);
@@ -91,6 +105,22 @@ public class PrimeiroVersoApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
---
+++ primeiroVerso/src/main/java/com/ceub/primeiro_verso/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.ceub.primeiro_verso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ceub.primeiro_verso.domain.Pedido;
+
+// obj da camada de acesso a dados referente a categoria
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+}
